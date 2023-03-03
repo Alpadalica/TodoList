32e7e662251579190dedba12eda91b8e66e531b7
frontcursed <ekvilaizer140@gmail.com>
2023-03-03 18:14:59 +0300

    add components and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import './App.css';
+import {TodoList} from "./TodoList";
 
 function App() {
+    let tasks1 = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+    ]
+    let tasks2 = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 2, title: 'XXX', isDone: false},
+        {id: 3, title: 'Wolf of the wall street', isDone: true},
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title='What to learn?' tasks={tasks1}/>
+            <TodoList title='Movie' tasks={tasks2}/>
         </div>
     );
 }

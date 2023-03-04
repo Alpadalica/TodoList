e935b7f9035379072b2a88e54c7e02d061c980e1
frontcursed <ekvilaizer140@gmail.com>
2023-03-04 19:35:13 +0300

    refactored add uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import React, {useState} from 'react';
 import './App.css';
 import {TaskType, TodoList} from "./TodoList";
+import {v1} from "uuid";
 
 
 export type FilterValueType = 'all' | 'completed' | 'active'
 
 function App() {
     let [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: 1, title: 'CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
+        {id: v1(), title: 'CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
     ]);
     let [filter, setFilter] = useState<FilterValueType>('all')
-    function removeTask(id: number) {
+    function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id !== id)
         setTasks(filteredTasks)
+    }
+    function addTask(title: string) {
+        let newTask = {
+            id: v1(),
+            title: title,
+            isDone: false
+        };
+        let newTasks = [newTask, ...tasks];
+        setTasks(newTasks)
     }
 
     function changeFilter (value: FilterValueType) {
@@ -36,6 +46,7 @@ function App() {
                 tasks={tasksForTodoList}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
+                addTask={addTask}
             />
         </div>
     );
